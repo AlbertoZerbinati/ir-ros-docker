6f1ede8f5c8ad032ba18b7e8bb003cb5c3684b2c
Alberto Zerbinati <albz@bendingspoons.com>
2022-11-21 15:21:43 +0000

    feat: exercise 4.1B

--- ir/exercises_ws/src/exercise_4_1/src/sub.cpp
+++ ir/exercises_ws/src/exercise_4_1/src/sub.cpp
@@ -3,18 +3,22 @@
 #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/Point.h>
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 
 int counter;
 
 void messageCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr &msg) {
-    counter++;
-    geometry_msgs::Point_<std::allocator<void>> pose = 
-        msg->detections[0].pose.pose.pose.position;
-    ROS_INFO("received pose #%d: (x=%f, y=%f, z=%f)", counter, pose.x, pose.y, pose.z);
-
-    // tf2_ros::Buffer tfBuffer;
-    // tf2_ros::TransformListener tfListener(tfBuffer);
-    // tfBuffer.lookupTransform("base_link", "rgb_camera_link", ros::Time(0));
+    for (apriltag_ros::AprilTagDetection detection : msg->detections) {
+        counter++;
+        geometry_msgs::Point_<std::allocator<void>> pose = detection.pose.pose.pose.position;
+        ROS_INFO("\nreceived pose #%d: (x=%f, y=%f, z=%f)", counter, pose.x, pose.y, pose.z);
+        
+        tf2_ros::Buffer tfBuffer;
+        tf2_ros::TransformListener tfListener(tfBuffer);
+        geometry_msgs::TransformStamped tf = tfBuffer.lookupTransform("base_link", "rgb_camera_link", ros::Time(0), ros::Duration(3));
+        tf2::doTransform(pose, pose, tf);
+        ROS_INFO("\ntransformed pose #%d: (x=%f, y=%f, z=%f)\n-------", counter, pose.x, pose.y, pose.z);
+    }
 }
 
 int main(int argc, char **argv) {
